2ca3713be032313981d47750c4b4eeff8ec99d9b
Jeniear <135406459+Jienniers@users.noreply.github.com>
2025-07-09 01:02:05 +0500

    Install Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,21 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: {
-      sourceType: "commonjs",
+      sourceType: 'commonjs',
       globals: {
-        ...globals.node,     // ✅ Includes process, __dirname, etc.
-        ...globals.es2021,   // ✅ ES2021 globals like Promise, etc.
+        ...globals.node, // ✅ Includes process, __dirname, etc.
+        ...globals.es2021, // ✅ ES2021 globals like Promise, etc.
       },
     },
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended', 'prettier'],
   },
   {
-    ignores: ["node_modules", ".env", "eslint.config.mjs"],
+    ignores: ['node_modules', '.env', 'eslint.config.mjs'],
   },
 ]);
