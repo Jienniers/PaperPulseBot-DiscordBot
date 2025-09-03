1616e01811f0bb5a1830df100581dd616f09f32f
Jeniear <135406459+Jienniers@users.noreply.github.com>
2025-09-03 16:18:04 +0500

    fix(mongoConnection): remove deprecated MongoDB connection options to eliminate warnings

--- utils/database/mongoConnection.js
+++ utils/database/mongoConnection.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectToMongoDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URL);
         console.log('✅ Connected to MongoDB');
     } catch (error) {
         console.error('❌ MongoDB connection error:', error);
