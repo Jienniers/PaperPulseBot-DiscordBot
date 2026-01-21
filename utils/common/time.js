9f243f0e580e866f8c46445b39dc3394cded64e6
Jeniear <135406459+Jienniers@users.noreply.github.com>
2026-01-21 13:12:34 +0500

    chore(time.js): convert to ESM

--- utils/common/time.js
+++ utils/common/time.js
@@ -1,7 +1,5 @@
-function formatPaperTime(minutesTotal) {
+export function formatPaperTime(minutesTotal) {
     const hours = Math.floor(minutesTotal / 60);
     const minutes = minutesTotal % 60;
     return `${hours} hour${hours !== 1 ? 's' : ''} ${minutes} minute${minutes !== 1 ? 's' : ''}`;
 }
-
-module.exports = { formatPaperTime };
