ff8f0b8fd4cd5ed468fa2971aa763f7e16b1602b
Jeniear <135406459+Jienniers@users.noreply.github.com>
2026-06-01 14:41:25 +0500

    Update deploy-slash-commands.js

--- scripts/deploy-slash-commands.js
+++ scripts/deploy-slash-commands.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 import { REST, Routes } from 'discord.js';
 
-import commands from '../commands/slashCommands/definitions.js';
+import commands from '../commands/slashCommands/utils/definitions.js';
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
